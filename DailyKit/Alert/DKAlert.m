0b80ae36bd49c0a43a8224e21a5c6bbe7772521d
pp <iospp@foxmail.com>
2018-02-01 11:19:32 +0800

    no message

--- DailyKit/Alert/DKAlert.m
+++ DailyKit/Alert/DKAlert.m
@@ -13,8 +13,11 @@
 + (void)alertWithMessage:(NSString *)message handler:(void (^)(UIAlertAction *action))handler viewController:(UIViewController *)viewController
 {
     UIAlertController * alert = [UIAlertController alertControllerWithTitle:nil message:message preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:nil];
-    [alert addAction:cancelAction];
+    
+    if (handler) {
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:cancelAction];
+    }
     
     UIAlertAction *OKAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:handler];
     [alert addAction:OKAction];
